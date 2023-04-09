d0b6614adbc726b39177b344e549ca1e19e19dc4
None Rival <none.rival.pro@gmail.com>
2023-04-09 14:34:44 +0200

    aim trainer is working, need it prettier, will add timer, cpm miss count mayber accruacy

--- aimTrain.js
+++ aimTrain.js
@@ -1,4 +1,4 @@
-const fireRange = document.querySelector('#fire-range');
+// const fireRange = document.querySelector('#fire-range');
 const aim = document.querySelector('circle');
 const svgBox = document.querySelector('svg')
 
@@ -6,14 +6,18 @@ aim.addEventListener("click", hit);
 
 let x;
 let y;
-const { width, height } = fireRange;
+const { clientWidth, clientHeight } = svgBox;
 const { cx, cy } = aim;
 
-
 function hit() {
+    x = Math.floor(Math.random() * clientWidth);
+    y = Math.floor(Math.random() * clientHeight);
+    aim.setAttribute('cx', x);
+    aim.setAttribute('cy', y);
+}
+
+svgBox.addEventListener('click', shoot)
+
+function shoot() {
     console.log('ok')
-    x = Math.floor(Math.random() * 100)
-    y = Math.floor(Math.random() * 100)
-    aim.cx = `${x}px`
-    aim.cy = `${y}px`
 }
